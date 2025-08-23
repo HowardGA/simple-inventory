f18cf1fc963ae42c26652b4b015a19b3e922caaa
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-08-22 18:29:30 -0700

    Cookie Settings

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -10,7 +10,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const cookieOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: None,
+    sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000,
     domain: '.onrender.com'
 };
